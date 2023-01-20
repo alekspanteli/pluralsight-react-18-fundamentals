014ad6f7399c5b49991aa86be54f1d64b7805fd4
alekspanteli <alekspanteli@rambler.ru>
2023-01-20 14:30:06 +0000

    add inlineStyles

--- components/banner.js
+++ components/banner.js
@@ -1,16 +1,21 @@
 import Image from "next/image";
 import GlobalLogo from "../public/GloboLogo.png";
+import styles from "./banner.module.css"
+
+const subtitleStyle = {
+  fontStyle: "italic",
+  color: "black"
+}
 
 const Banner = () => (
   <header>
-    <div className="grid grid-cols-[auto_1fr] items-center mb-4 pt-4">
+    <div className="grid sm:grid-cols-[auto_1fr] items-center mb-4 pt-4">
       <Image
         src={GlobalLogo}
         alt="Global Logo"
-        className="cursor-pointer"
-        width={150}
+        className={styles.logo}
       />
-      <h1 className="text-center">Some text goes here</h1>
+      <h1 className="sm:text-center" style={subtitleStyle}>Some text goes here</h1>
     </div>
   </header>
 );
